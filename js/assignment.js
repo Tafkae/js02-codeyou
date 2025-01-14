7c4a42dd53cbadc2b4af0c781a87fbbca22062e3
Lisa H. <neuroticNanobot@outlook.com>
2025-01-14 11:28:15 -0500

    completed!

--- js/assignment.js
+++ js/assignment.js
@@ -3,28 +3,36 @@
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
 const yearElement = document.getElementById("year");
-const resolutionElement = document.getElementById("resolution")
-const willMeetResolutionElement = document.getElementById("will-meet-resolution")
-const submissionBtn = document.getElementById("submission-btn")
+const resolutionElement = document.getElementById("resolution");
+const willMeetResolutionElement = document.getElementById(
+  "will-meet-resolution"
+);
+const submissionBtn = document.getElementById("submission-btn");
 
 // Declare your variables here.
+const resolution = "Get a better job!";
+
+const currentYear = (function () {
+  const today = new Date();
+  return today.getFullYear();
+})();
+
+let willMeetResolution = true;
 
 function updateYear() {
-    // write the logic
-
+  yearElement.innerText = currentYear;
 }
 
 function updateResolution() {
-    // Update this function 
-
+  resolutionElement.innerText = resolution;
+  willMeetResolutionElement.innerText = willMeetResolution ? "YEA" : "NAH";
 }
 
 function render() {
-    // Finish writing this function
-
+  updateYear();
+  updateResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
-    // Finish writing this function
-
-})
+  render();
+});
